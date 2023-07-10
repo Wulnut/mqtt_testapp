0f2bbc75ec7087c0879bd907270ad3faca17c3c2
LyZ <985951964@qq.com>
2023-07-10 18:27:53 +0800

    feat: mqtt lib

--- include/mqtt_client.h
+++ include/mqtt_client.h
@@ -5,7 +5,25 @@
 
 #define SSL_PATH "./conf/server.crt"
 
+typedef struct cts_mqtt_client {
+	MQTTAsync mqtt_client;
+	char query[128];
+	char query_res[128];
+	char cmd[128];
+	char cmd_res[128];
+	char plugin[128];
+	char plugin_res[128];
+	char report[128];
+	char report_res[128];
+	char report_rt[128];
+	char passowrd[128];
+	char username[128];
+    char host[128];
+} mqtt_info_t;
+
 static MQTTAsync_SSLOptions configure_ssl_opts();
 static MQTTAsync_connectOptions configure_conn_opts();
+
+void mqtt_run();
 
 #endif MQTT_CLIENT
--- include/util.h
+++ include/util.h
@@ -6,11 +6,16 @@
 #include <unistd.h>
 
 #define MAXSIZE 30
+#define MAX_LINE_LEN 1024
+#define CONFIG_PATH "./conf/test.conf"
 
 int count;
 char buff[MAXSIZE];
 char *lable = "\\/\\-\\/";
 
 void progress_bar(int flag);
+
+void testapp_init();
+void testapp_run();
 
 #endif
