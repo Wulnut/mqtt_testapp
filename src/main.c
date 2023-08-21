5564165c5a8b19134b8e8caecaa603f9fc4579ab
LyZ <985951964@qq.com>
2023-08-21 18:20:26 +0800

    style: 修复文件读取bug

--- src/main.c
+++ src/main.c
@@ -7,15 +7,19 @@
 mqtt_info_t mit;
 
 int main(int argc, char **argv) { 
+    int rc = 0;
 
     log_set_level(LOG_TRACE);
 
     process_signal_init();
 
-    //BUG
     opt_init(argc, argv);
 
     config_init(&mit);
+
+   rc = read_test_conf(&mit, "../conf/tianyi.conf");
+
+   log_debug("rc: %d", rc);
 
     // mqtt_run(&mit);
 
