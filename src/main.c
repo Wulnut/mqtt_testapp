3c37a0d07a4d218cfea256e6d1f758b9a8f80001
LyZ <985951964@qq.com>
2023-08-21 18:45:14 +0800

    style: 调试mqtt连接

--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@
 mqtt_info_t mit;
 
 int main(int argc, char **argv) { 
-    int rc = 0;
 
     log_set_level(LOG_TRACE);
 
@@ -17,11 +16,9 @@ int main(int argc, char **argv) {
 
     config_init(&mit);
 
-   rc = read_test_conf(&mit, "../conf/tianyi.conf");
+    read_test_conf(&mit, "../conf/tianyi.conf");
 
-   log_debug("rc: %d", rc);
-
-    // mqtt_run(&mit);
+    mqtt_run(&mit);
 
     return 0;
 }
