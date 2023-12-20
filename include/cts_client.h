e243804ea1db53fe73e5839aeda0fedffb387d8d
LyZ <carepdime@gmail.com>
2023-12-20 15:43:48 +0800

    feat: 新增可配置addr与port

--- include/cts_client.h
+++ include/cts_client.h
@@ -1,13 +1,21 @@
 #ifndef CTS_CLIENT_H
 #define CTS_CLIENT_H
 
+#define _GUN_SOURCE
+
 #include <libubox/uloop.h>
 #include <mosquitto.h>
 
-typedef struct cts_client
-{
+#define MAX_LINE_LEN 1024
+#define CONFIG_PATH  "../conf/ini.conf"
+#define DEFAULT_ADDR "101.227.231.138"
+#define DEFAULT_PORT "18080"
+
+typedef struct cts_client {
+    char                 addr[64];
+    char                 port[8];
     int                  retry_num;
-    struct mosquitto*    mosq;
+    struct mosquitto    *mosq;
     struct uloop_fd      mosquitto_ufd;
     struct uloop_timeout connect_timer;
 } cts_client_t;
--- src/cts_client.c
+++ src/cts_client.c
@@ -3,13 +3,15 @@
 #include <libubox/uloop.h>
 #include <mosquitto.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-cts_client_t      cc;
+cts_client_t     cc;
 struct list_head session;
 
 static void cc_retry_conn();
 
-static void on_connect(struct mosquitto* mosq, void* obj, int rc)
+static void on_connect(struct mosquitto *mosq, void *obj, int rc)
 {
     if (rc) {
         printf("Error with result code: %d\n", rc);
@@ -21,21 +23,27 @@ static void on_connect(struct mosquitto* mosq, void* obj, int rc)
     }
 }
 
-static void on_message(struct mosquitto* mosq, void* obj, const struct mosquitto_message* msg)
+static void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *msg)
 {
     if (msg->payload) {
-        printf("Received message on topic %s: %s\n", msg->topic, (char*)msg->payload);
+        printf("Received message on topic %s: %s\n", msg->topic, (char *)msg->payload);
     }
 }
 
-void mosquitto_fd_handler(struct uloop_fd* u, unsigned int events)
+static void on_log(struct mosquitto *mosq, void *userdata, int level, const char *str)
+{
+    printf("mosquitto log: %s\n", str);
+}
+
+static void mosquitto_fd_handler(struct uloop_fd *u, unsigned int events)
 {
     int fd = mosquitto_socket(cc.mosq);
-    if (fd == -1) return;
+    if (fd == -1)
+        return;
 
     // 调用 mosquitto_loop 来处理事件
-    mosquitto_loop_read(cc.mosq, 1);
-    mosquitto_loop_write(cc.mosq, 1);
+    mosquitto_loop_read(cc.mosq, 1024);
+    mosquitto_loop_write(cc.mosq, 1024);
     mosquitto_loop_misc(cc.mosq);
 }
 
@@ -54,9 +62,9 @@ static void cc_connect()
 
     mosquitto_connect_callback_set(cc.mosq, on_connect);
     mosquitto_message_callback_set(cc.mosq, on_message);
+    mosquitto_log_callback_set(cc.mosq, on_log);
 
-    if ((rc = mosquitto_tls_set(cc.mosq, "../conf/zxykey.cer", NULL, NULL, NULL, NULL)) !=
-        MOSQ_ERR_SUCCESS) {
+    if ((rc = mosquitto_tls_set(cc.mosq, CONFIG_PATH, NULL, NULL, NULL, NULL)) != MOSQ_ERR_SUCCESS) {
         printf("Failed to mosquitto_tls_set: %s (%d)\n", mosquitto_strerror(rc), rc);
         cc_retry_conn();
     }
@@ -66,7 +74,7 @@ static void cc_connect()
         cc_retry_conn();
     }
 
-    if ((rc = mosquitto_connect(cc.mosq, "101.227.231.138", 18080, 30)) != MOSQ_ERR_SUCCESS) {
+    if ((rc = mosquitto_connect(cc.mosq, cc.addr, atoi(cc.port), 30)) != MOSQ_ERR_SUCCESS) {
         printf("Unable to connect\n");
         cc_retry_conn();
     }
@@ -95,20 +103,80 @@ static void cc_retry_conn()
 {
     printf("(\"CTS\") %dth retry auth\n", ++cc.retry_num);
 
-    if (cc.retry_num <= 3) { cc_reconnect(1000); }
+    if (cc.retry_num <= 3) {
+        cc_reconnect(1000);
+    }
     else {
         cc_reconnect(10000);
     }
 }
 
-static void connect_cb(struct uloop_timeout* timeout)
+static void connect_cb(struct uloop_timeout *timeout) { cc_connect(); }
+
+static int config_init()
 {
-    cc_connect();
+    FILE *fp         = NULL;
+    char  line[1024] = "";
+
+    fp = fopen(CONFIG_PATH, "r");
+
+    if (fp == NULL) {
+        printf("ini.conf open failed\n");
+        goto err;
+    }
+
+    while (fgets(line, 1024, fp) != NULL) {
+        char  key[MAX_LINE_LEN]   = "";
+        char  value[MAX_LINE_LEN] = "";
+        char *ptr                 = NULL;
+
+        // replace the '\r' in the read line
+        ptr = strchr(line, '\r');
+        if (ptr)
+            *ptr = '\0';
+
+        if (line[0] == '#' || (line[0] == '/' && line[1] == '/') || line[0] == '\0') {
+            continue;
+        }
+
+        if (sscanf(line, "%[^=] = %[^\n]", key, value) != 2) {
+            continue;
+        }
+
+        for (int i = strlen(key) - 1; i >= 0 && key[i] == ' '; --i) {
+            key[i] = '\0';
+        }
+
+        if (strcmp(key, "ip") == 0) {
+            strncpy(cc.addr, value, strlen(value) + 1);
+            continue;
+        }
+
+        if (strcmp(key, "port") == 0) {
+            strncpy(cc.port, value, strlen(value) + 1);
+        }
+    }
+
+    fclose(fp);
+    return 1;
+
+err:
+    fclose(fp);
+    return 0;
 }
 
 void cc_init()
 {
-    if (mosquitto_lib_init() < 0) printf("mosquitto lib init failed\n");
+    if (config_init() == 0)
+        printf("config init failed\n");
+
+    if (strlen(cc.addr) == 0 || strlen(cc.port) == 0) {
+        strncpy(cc.addr, DEFAULT_ADDR, sizeof(DEFAULT_ADDR));
+        strncpy(cc.port, DEFAULT_PORT, sizeof(DEFAULT_PORT));
+    }
+
+    if (mosquitto_lib_init() < 0)
+        printf("mosquitto lib init failed\n");
 
     cc.retry_num        = 1;
     cc.connect_timer.cb = connect_cb;
@@ -116,10 +184,7 @@ void cc_init()
     INIT_LIST_HEAD(&session);
 }
 
-void cc_run()
-{
-    cc_connect();
-}
+void cc_run() { cc_connect(); }
 
 void cc_done()
 {
