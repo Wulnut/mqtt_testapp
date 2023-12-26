e45acffc45fdf0c0cd52c80a8b60534de5d8a17f
LyZ <carepdime@gmail.com>
2023-12-26 18:10:29 +0800

    feat: 新增安全释放安全分配

--- include/common.h
+++ include/common.h
@@ -7,6 +7,16 @@
 #define ULOG_DEBUG(fmt, ...) ulog("DEBUG", fmt, ##__VA_ARGS__)
 #define ULOG_MARK()          ulog("DEBUG", "%s %d\n", __FUNCTION__, __LINE__)
 
+#define _calloc(x) ucalloc(x, __FILE__, __LINE__)
+#define _new(type) _calloc(sizeof(type))
+#define _free(x)         \
+    do {                 \
+        if (x != NULL) { \
+            free(x);     \
+            x = NULL;    \
+        }                \
+    } while (0)
+
 void ulog(const char *priority, const char *fmt, ...);
 
 #endif
