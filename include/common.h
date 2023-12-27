ba695acebee6f87081f4e3264cf8edf0e0a69d17
LyZ <carepdime@gmail.com>
2023-12-27 19:30:01 +0800

    style: 删除不必要的宏

--- include/common.h
+++ include/common.h
@@ -7,8 +7,6 @@
 #define ULOG_DEBUG(fmt, ...) ulog("DEBUG", fmt, ##__VA_ARGS__)
 #define ULOG_MARK()          ulog("DEBUG", "%s %d\n", __FUNCTION__, __LINE__)
 
-#define _calloc(x) ucalloc(x, __FILE__, __LINE__)
-#define _new(type) _calloc(sizeof(type))
 #define _free(x)         \
     do {                 \
         if (x != NULL) { \
